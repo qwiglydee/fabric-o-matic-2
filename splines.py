73ba4196bf7ee5c7b1e7780eaf2bf12510d827c1
qMax <qwiglydee@gmail.com>
2026-06-30 05:34:01 +0700

    cleanup splines api

--- splines.py
+++ splines.py
@@ -3,42 +3,72 @@
 import numpy as np
 from numpy.typing import NDArray
 
-M1 = np.array([[1, 0], [-1, 1]])
+# blending matrices for uniform splines
+BM = {
+    1: np.array([[1, 0], [-1, 1]]),
+    2: np.array([[1, 1, 0], [-2, 2, 0], [1, -2, 1]]) / 2,
+    3: np.array([[1, 4, 1, 0], [-3, 0, 3, 0], [3, -6, 3, 0], [-1, 3, -3, 1]]) / 6,
+}
 
-M2 = np.array([[1, 1, 0], [-2, 2, 0], [1, -2, 1]]) / 2
-
-M3 = np.array([[1, 4, 1, 0], [-3, 0, 3, 0], [3, -6, 3, 0], [-1, 3, -3, 1]]) / 6
+# matrices for curve derivative (direction) for powers(d-1)
+BMdt = {
+    1: np.array([[-1, 1]]),
+    2: np.array([[-1, 1, 0], [1, -2, 1]]),
+    3: np.array([[-1, 0, 1, 0], [2, -4, 2, 0], [-1, 3, -3, 1]]) / 2,
+}
 
 
 def powers_t(d: int, t: float):
-    """Power vector: [1, t, ... t^2]"""
-    exps = np.arange(d + 1, dtype=float)
+    exps = np.arange(d + 1)
     return t**exps
 
 
-def powers(d: int, t: NDArray):
-    """Power vector: [1, t, ... t^2]"""
-    exps = np.arange(d + 1, dtype=float)
+def powers_tt(d: int, t: NDArray):
+    exps = np.arange(d + 1)
     return t[..., None] ** exps[None, ...]
 
 
-def segm_points(d: int, points: NDArray, i: int):
-    """Points for a segment: C_(i-d)^n"""
+def splines_t(d: int, t: float):
+    """[B_i-d ... B_i] = [0, 1, ... t^d] M"""
+    return powers_t(d, t) @ BM[d]
+
+
+def splines_tt(d: int, tt: NDArray):
+    """[B_i-d ... B_i] = [0, 1, ... t^d] M"""
+    return powers_tt(d, tt) @ BM[d]
+
+
+def controls_i(d: int, points: NDArray, i: int):
+    """Slice of control points for i'th span of t
+    i.e. for a spline starting at t_i
+    """
     assert i >= d and i < len(points)
     return points[i - d : i + 1]
 
 
-def curve_t(d: int, M: NDArray, points: NDArray, t):
-    assert points.shape[0] == d + 1
-    return powers_t(d, t) @ M @ points
+def curve_t(d: int, controls: NDArray, t: float):
+    """A point on curve at t, with 0 < t < 1"""
+    assert controls.shape[0] == d + 1
+    return powers_t(d, t) @ BM[d] @ controls
 
 
-def segm_curve(d: int, M: NDArray, points: NDArray, i: int, tt: NDArray):
-    """Curve for a segment: P M C"""
-    return powers(d, tt) @ M @ segm_points(d, points, i)
+def curve_tt(d: int, controls: NDArray, tt: NDArray):
+    """A segment of curve over tt, with 0 < t < 1"""
+    assert controls.shape[0] == d + 1
+    return powers_tt(d, tt) @ BM[d] @ controls
 
 
-def mega_curve(d, M, points, tt):
-    """Curve for all along"""
-    pm = powers(d, tt) @ M
-    return np.concat([pm @ segm_points(d, points, j) for j in range(d, len(points))])
+def megacurve(d: int, points: NDArray, tt: NDArray):
+    wm = powers_tt(d, tt) @ BM[d]
+    return np.concat([wm @ controls_i(d, points, i) for i in range(d, len(points))])
+
+
+def flow_t(d: int, controls: NDArray, t: float):
+    """A direction of curve at t, with 0 < t < 1"""
+    assert controls.shape[0] == d + 1
+    return powers_t(d - 1, t) @ BMdt[d] @ controls
+
+
+def megaflow(d: int, points: NDArray, tt: NDArray):
+    wm = powers_tt(d - 1, tt) @ BMdt[d]
+    return np.concat([wm @ controls_i(d, points, i) for i in range(d, len(points))])
